ad6a3847b8a3f3e8b085d27c955c860c922e900d
anuj dubey <anujd5310@gmail.com>
2026-07-07 19:44:47 +0530

    Add files via upload

---
+++ medicine.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.core.database import Base
+
+class Medicine(Base):
+    __tablename__ = "medicines"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150))
+    category = Column(String(80))
+    unit = Column(String(20))
