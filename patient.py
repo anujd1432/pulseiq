ad520ccde222000189f27e860e8cbfecfa96d2de
anuj dubey <anujd5310@gmail.com>
2026-07-08 23:03:41 +0530

    Add files via upload

---
+++ patient.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(150))
+    age = Column(Integer)
+    gender = Column(String(10))
+    contact = Column(String(20))
+    address = Column(String)
+    blood_group = Column(String(5))
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
