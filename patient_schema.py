6d5dad85aba199678d43c72ceb29543b82e8eda8
anuj dubey <anujd5310@gmail.com>
2026-07-14 22:34:41 +0530

    Add files via upload

---
+++ patient_schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class PatientCreate(BaseModel):
+    full_name: str
+    age: int
+    gender: str
+    contact: str
+    address: Optional[str] = None
+    blood_group: Optional[str] = None
+
+class PatientUpdate(BaseModel):
+    full_name: Optional[str] = None
+    age: Optional[int] = None
+    gender: Optional[str] = None
+    contact: Optional[str] = None
+    address: Optional[str] = None
+    blood_group: Optional[str] = None
+
+class PatientOut(BaseModel):
+    id: int
+    full_name: str
+    age: int
+    gender: str
+    contact: str
+    address: Optional[str] = None
+    blood_group: Optional[str] = None
+
+    class Config:
+        from_attributes = True
