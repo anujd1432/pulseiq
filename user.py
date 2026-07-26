1e3701f8b4b063f6adb39f4e5c2023bec809d6b7
anuj dubey <anujd5310@gmail.com>
2026-07-26 21:54:36 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy.sql import func
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(150), nullable=False)
+    email = Column(String(150), unique=True, nullable=False, index=True)
+    password_hash = Column(String, nullable=False)
+    role = Column(String(20), nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
