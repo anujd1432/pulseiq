23387dcc94e3cf582f6867828e9d0f3b45defcb4
anuj dubey <anujd5310@gmail.com>
2026-07-30 18:17:57 +0530

    Add files via upload

---
+++ hospital_daily_stats.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, Date
+from app.core.database import Base
+
+class HospitalDailyStats(Base):
+    __tablename__ = "hospital_daily_stats"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date, unique=True, nullable=False)
+    patient_inflow = Column(Integer, nullable=False)
+    icu_occupied_beds = Column(Integer, nullable=False)
+    total_occupied_beds = Column(Integer, nullable=False)
